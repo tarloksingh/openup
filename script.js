ec891d0b0ab5ab5ac661be1d19eaa79b0fecf452
tarloksingh <73892069+tarloksingh@users.noreply.github.com>
2022-08-27 11:29:12 -0500

    autoplay when on screen, muted removed

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementsById('anim'),
-    rederer: 'svg',
-    loop:true,
-    autoplay:true,
-    path: 'People.json'
-
-})
+let video = document.querySelector('video');
+let isPaused = false; /* Flag for auto-paused video */
+let observer = new IntersectionObserver((entries, observer) => { 
+  entries.forEach(entry => {
+    if(entry.intersectionRatio!=1  && !video.paused){
+      video.pause(); isPaused = true;
+    }
+    else if(isPaused) {video.play(); isPaused=false}
+  });
+}, {threshold: 1});
+observer.observe(video);
